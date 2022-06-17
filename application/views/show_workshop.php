68d1ca7d178914d03c7360687f5f0262f24c90cd
Christian Balidoy <redchristian20@gmail.com>
2022-06-17 15:35:36 +0800

    jhdjcc

--- application/views/show_workshop.php
+++ application/views/show_workshop.php
@@ -1,7 +1,7 @@
 <div class="workshop-details-container-card">
 
   <h3>Workshop Details</h3>
-  <p><img src="../uploads/<?= $workshop['event_poster_link']?>" class="img-fluid" alt="Image"></p>
+  <p><img src="../uploads/<?= $workshop['event_poster_link']?>" alt="Image" class="workshop-banner"></p>
   <p><strong>Name: </strong><?=$workshop['workshop_name']?></p>
   <p><strong>Description: </strong><?=$workshop['workshop_description']?></p>
   <p><strong>Venue: </strong><?=$workshop['venue']?></p>
@@ -9,6 +9,7 @@
   <p><strong>End date: </strong><?=$workshop['end_date']?></p>
   <p><strong>Start: </strong><?=date('h:i A',strtotime($workshop['start_time']))?></p>
   <p><strong>End: </strong><?=date('h:i A',strtotime($workshop['end_time']))?></p>
+  <p><img src="<?=$workshop['qr_code_link']?>"></p>
   <form action="confirm_workshop" method="post">
         <input type="hidden" name="form_submitted" value="1" />
         <input type="submit" name="add_participants" value="Add Participants">
--- application/views/workshops.php
+++ application/views/workshops.php
@@ -1,25 +1,30 @@
 
     <h1>Workshops</h1>
-    <div class='.workshop-details-container-card'>
+    
 <?php
         if(isset($workshops)){
             foreach($workshops as $key => $val)
             {?>
-                <tr>
-                    <p><?=$val['workshop_name']?></p>
-                    <p><?=$val['workshop_description']?></p>
-                    <p><?=$val['venue']?></p>
-                    <p><?=$val['start_date']?></p>
-                    <p><?=$val['end_date']?></p>
-                    <p><img src="./uploads/<?= $val['event_poster_link']?>" class="img-fluid" alt="Image" width="960" height="540"></p>
-                    <p><?=date('h:i A',strtotime($val['start_time']))?></p>
-                    <p><?=date('h:i A',strtotime($val['end_time']))?></p>
-                    <p>
-                        <a href='show_workshop/<?=$val['id']?>'>Show</a> | <a href='edit_workshop/<?=$val['id']?>'>Edit</a>
-                    </p>
+                <div class='workshop-details-container-card'>
+                    <p><img src="./uploads/<?= $val['event_poster_link']?>" alt="Image" class="workshop-banner"></p>
+                    <p><strong>Name: </strong><?=$val['workshop_name']?></p>
+                    <p><strong>Description: </strong><?=$val['workshop_description']?></p>
+                    <p><strong>Venue: </strong><?=$val['venue']?></p>
+                    <p><strong>Start date: </strong><?=$val['start_date']?></p>
+                    <p><strong>End date: </strong><?=$val['end_date']?></p>
+                    <p><strong>Start: </strong><?=date('h:i A',strtotime($val['start_time']))?></p>
+                    <p><strong>End: </strong><?=date('h:i A',strtotime($val['end_time']))?></p>
                     <p><img src="<?=$val['qr_code_link']?>" class="img-fluid"></p>
-                </tr>
+
+                    <form action="show_workshop/<?=$val['id']?>" method="post">
+                        <input type="submit" name="show_workshop" value="Show">
+                    </form>
+
+                    <form action="edit_workshop/<?=$val['id']?>" method="post">
+                        <input type="submit" name="edit_workshop" value="Edit">
+                    </form>
+                </div>
 <?php       }
         }
 ?>
-    </div>
+    
