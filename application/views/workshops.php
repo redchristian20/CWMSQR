1b0bd730fcdb5f2b311e1eb23037fb3233bc3b90
Christian Balidoy <redchristian20@gmail.com>
2022-06-20 07:49:19 +0800

    a

--- application/views/workshops.php
+++ application/views/workshops.php
@@ -1,10 +1,16 @@
 
+<<<<<<< Updated upstream
     <h1>Workshops</h1>
     
+=======
+    
+    <div class='workshop-section'>
+>>>>>>> Stashed changes
 <?php
         if(isset($workshops)){
             foreach($workshops as $key => $val)
             {?>
+<<<<<<< Updated upstream
                 <div class='workshop-details-container-card'>
                     <p><img src="./uploads/<?= $val['event_poster_link']?>" alt="Image" class="workshop-banner"></p>
                     <p><strong>Name: </strong><?=$val['workshop_name']?></p>
@@ -28,3 +34,25 @@
         }
 ?>
     
+=======
+                <h1>Workshops</h1>
+                <p><img src="./uploads/<?= $val['event_poster_link']?>" class="img-fluid" alt="Image" width="960" height="540"></p>
+                <p><?=$val['workshop_name']?></p>
+                <p><?=$val['workshop_description']?></p>
+                <p><?=$val['venue']?></p>
+                <p><?=$val['start_date']?></p>
+                <p><?=$val['end_date']?></p>
+                <p><?=date('h:i A',strtotime($val['start_time']))?></p>
+                <p><?=date('h:i A',strtotime($val['end_time']))?></p>
+                <p><img src="<?=$val['qr_code_link']?>" class="img-fluid" alt="Image"></p>
+                <p>
+                    <a href='show_workshop/<?=$val['id']?>'>Show</a> | <a href='edit_workshop/<?=$val['id']?>'>Edit</a>
+                </p>
+<?php       }
+        }
+?>
+    </div>
+    <form action='add_workshop' method='POST'>
+        <input type='submit' value='Add Workshop'>
+    </form>
+>>>>>>> Stashed changes
