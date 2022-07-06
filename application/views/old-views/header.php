f80cf7e93c6ee1da0a345e38cf4ffcb79390cf98
Christian Balidoy <redchristian20@gmail.com>
2022-07-06 17:31:32 +0800

    asdjkhbajskhdb

--- application/views/old-views/header.php
+++ application/views/old-views/header.php
@@ -36,10 +36,12 @@
                     <a class="nav-link" href="add_workshop">Add workshop</a>
                 </li>
             </ul>
-            <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-            </form>
+            <ul class="form-inline my-2 my-lg-0">
+                <li class="nav-item active">
+                    <a class="nav-link" href="home">Home <span class="sr-only">(current)</span></a>
+                </li>
+            </ul>
+
         </div>
     </nav>
 
