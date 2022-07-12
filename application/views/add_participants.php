f2213b9c9e4c7a517e0c029d20e8c7a5ab3ab616
Christian Balidoy <redchristian20@gmail.com>
2022-07-12 20:55:21 +0800

    a

--- application/views/add_participants.php
+++ application/views/add_participants.php
@@ -1,17 +1,59 @@
-<?php
-        ini_set('auto_detect_line_endings', true);
-?>
+<div class="container box">
+  <h3>How to Import CSV Data into Mysql using Codeigniter</h3>
+  <br />
 
-<div class='container align-items-center'>
+  <form method="post" id="import_csv" enctype="multipart/form-data">
+   <div class="form-group">
+    <label>Select CSV File</label>
+    <input type="file" name="csv_file" id="csv_file" required accept=".csv" />
+   </div>
+   <br />
+   <button type="submit" name="import_csv" class="btn btn-info" id="import_csv_btn">Import CSV</button>
+  </form>
+  <br />
+  <div id="imported_csv_data"></div>
+ </div>
+</body>
+</html>
 
-    <form action="upload.php" method="post" enctype="multipart/form-data">
-        <label for="fileToUpload">Select File:</label>
-        <input type="file" name="fileToUpload" id="fileToUpload">
-        <input type='hidden' name='action' value='upload_file'>
-        <input type="submit" value="Submit"/>
-    </form>
+<script>
+$(document).ready(function(){
 
-    <?php if(isset($errors)){echo $errors;}?>
-    <?php if(isset($image_error)){echo $image_error;}?>
-    <?php if(isset($success)){echo $success;}?>
-</div>
+ load_data();
+
+ function load_data()
+ {
+  $.ajax({
+   url:"<?php echo base_url(); ?>csv_import/load_data",
+   method:"POST",
+   success:function(data)
+   {
+    $('#imported_csv_data').html(data);
+   }
+  })
+ }
+
+ $('#import_csv').on('submit', function(event){
+  event.preventDefault();
+  $.ajax({
+   url:"<?php echo base_url(); ?>csv_import/import",
+   method:"POST",
+   data:new FormData(this),
+   contentType:false,
+   cache:false,
+   processData:false,
+   beforeSend:function(){
+    $('#import_csv_btn').html('Importing...');
+   },
+   success:function(data)
+   {
+    $('#import_csv')[0].reset();
+    $('#import_csv_btn').attr('disabled', false);
+    $('#import_csv_btn').html('Import Done');
+    load_data();
+   }
+  })
+ });
+ 
+});
+</script>
