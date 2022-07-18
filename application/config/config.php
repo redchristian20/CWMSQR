beea24bc8ef1236261c28a0a6483f8d6ad4cfa62
Christian Balidoy <redchristian20@gmail.com>
2022-07-18 17:54:39 +0800

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -22,7 +22,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://cwmsqr-uc.herokuapp.com';
+$config['base_url'] = 'https://localhost';
 /*
 |--------------------------------------------------------------------------
 | Index File
