21f50b5c12edd2bed9a4c00211e8749263fcc634
Adamoulis <106956075+Adamoulis@users.noreply.github.com>
2022-07-22 10:50:50 +0800

    Update Main.php

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Main extends CI_Controller {
 
-    public function parent::__construct()
+    public function __construct()
     {
             parent::__construct();
             $this->load->helper(array('form', 'url'));
