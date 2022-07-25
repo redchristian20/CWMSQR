b2e66e129435cc48616e8c9af608c5c670edbc91
Adamoulis <106956075+Adamoulis@users.noreply.github.com>
2022-07-25 15:00:42 +0800

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -20,9 +20,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 | If you need to allow multiple domains, remember that this file is still
 | a PHP script and you can easily do that on your own.
-|
+|https://cwmsqr-uc.herokuapp.com/
 */
-$config['base_url'] = 'https://cwmsqr-uc.herokuapp.com/';
+$config['base_url'] = 'localhost';
 /*
 |--------------------------------------------------------------------------
 | Index File
